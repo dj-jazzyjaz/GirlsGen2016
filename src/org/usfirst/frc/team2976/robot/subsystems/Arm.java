b2c248ed32c988a692a1e20ed3c2739ecb358bec
Neil Hazra <neilhazra001@gmail.com>
2016-08-27 12:19:20 -0700

    Added Roller

--- src/org/usfirst/frc/team2976/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team2976/robot/subsystems/Arm.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team2976.robot.subsystems;
 
+import org.usfirst.frc.team2976.robot.Robot;
 import org.usfirst.frc.team2976.robot.RobotMap;
 import org.usfirst.frc.team2976.robot.commands.ArmControl;
 
@@ -7,6 +8,7 @@ import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import util.PIDMain;
 import util.PIDSource;
 
@@ -14,9 +16,9 @@ import util.PIDSource;
  *
  */
 public class Arm extends Subsystem {
-	public CANTalon rightArm;
-	public CANTalon leftArm;
-	public Talon roller;
+	private CANTalon rightArm;
+	private CANTalon leftArm;
+	
 	private Encoder rightArmEncoder, leftArmEncoder;
 
 	public PIDSource rightArmPIDSource;
@@ -25,44 +27,64 @@ public class Arm extends Subsystem {
 	public PIDMain rightArmPID;
 	public PIDMain leftArmPID;
 
-	public Arm()	{
-		double kp = 0.1;
-		double ki = 0;
-		double kd = 0;
-		
-    	rightArm =  new CANTalon(RobotMap.RightArmMotor);
-    	leftArm = new CANTalon (RobotMap.LeftArmMotor);
-    	leftArmEncoder = new Encoder(RobotMap.LeftArmEncoderA,RobotMap.LeftArmEncoderB);
-    	rightArmEncoder = new Encoder(RobotMap.RightArmEncoderA,RobotMap.RightArmEncoderB); 
-    	
-    	rightArmPIDSource = new PIDSource()	{
+	private double kp = 0;
+	private double ki = 0;
+	private double kd = 0;
+
+	public boolean overAmped = false;
+	public final double maxCurrent = 5;
+
+	public Arm() {
+		rightArm = new CANTalon(RobotMap.RightArmMotor);
+		leftArm = new CANTalon(RobotMap.LeftArmMotor);
+
+		leftArmEncoder = new Encoder(RobotMap.LeftArmEncoderA, RobotMap.LeftArmEncoderB);
+		rightArmEncoder = new Encoder(RobotMap.RightArmEncoderA, RobotMap.RightArmEncoderB);
+
+		rightArmPIDSource = new PIDSource() {
 			public double getInput() {
 				return rightArmEncoder.get();
 			}
 		};
-		leftArmPIDSource = new PIDSource()	{
+		leftArmPIDSource = new PIDSource() {
 			public double getInput() {
 				return leftArmEncoder.get();
 			}
 		};
-		
+
 		rightArmPID = new PIDMain(rightArmPIDSource, 0, 100, kp, ki, kd);
 		leftArmPID = new PIDMain(leftArmPIDSource, 0, 100, kp, ki, kd);
 		rightArmPID.isEnabled(true);
 		leftArmPID.isEnabled(true);
 	}
-	
-	public void setPosition(double position)	{
+
+	public void setPosition(double position) {
 		rightArmPID.setSetpoint(position);
 		leftArmPID.setSetpoint(position);
-		setPower(rightArmPID.getOutput(),leftArmPID.getOutput());
+		setPower(rightArmPID.getOutput(), leftArmPID.getOutput());
 	}
-	public void setPower(double rightPower, double leftPower)	{
-		rightArm.set(rightPower);
-		leftArm.set(leftPower);
+
+	public void setPower(double rightPower, double leftPower) {
+		SmartDashboard.putNumber("ArmOutputCurrentAverage",
+				(leftArm.getOutputCurrent() + rightArm.getOutputCurrent()) / 2);
+		
+		if (rightArm.getOutputCurrent() > maxCurrent || leftArm.getOutputVoltage() > maxCurrent) {
+			overAmped = true;
+		}
+		if (Robot.oi.armStick.getPOV() != -1) {
+			overAmped = false;
+		}
+		if (!overAmped) {
+			rightArm.set(rightPower / 3);
+			leftArm.set(leftPower / 3);
+		} else {
+			rightArm.set(0);
+			leftArm.set(0);
+		}
+
 	}
+
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
 		setDefaultCommand(new ArmControl());
 	}
 }
